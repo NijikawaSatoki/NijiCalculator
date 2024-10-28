e636ee31b82c7d6ecb33b8b9af2e507d3e11d109
Satoki Nijikawa <120293916+NijikawaSatoki@users.noreply.github.com>
2024-10-28 15:45:22 -0400

    Create MainProgram.cs

---
+++ src/MainProgram.cs
@@ -0,0 +1,8 @@
+using System;
+namespace NijiCalculator {
+    class MainProgram {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World.\nThis is placeholder text :3");
+        }
+    }
+}
