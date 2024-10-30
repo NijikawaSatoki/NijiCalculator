eecd19150641bb16824ac12dccd5dd14d6c144a2
NijikawaSatoki <nijikawasatoki00@outlook.com>
2024-10-30 01:25:31 -0400

    Create Calculate.cs

---
+++ src/NijiCalculator/Calculate.cs
@@ -0,0 +1,34 @@
+/* =============================================================================
+ * Class file `Calculate'
+ * =============================================================================
+ * 
+ * Class author: Niji System
+ * 
+ */
+
+using System;
+
+namespace NijiCalculator {
+    class Calculate {
+        public long Add(long x, long y) {
+            long z = x + y;
+            return z;
+        }
+        public long Subtract(long x, long y) {
+            long z = x - y;
+            return z;
+        }
+        public long Multiply(long x, long y) {
+            long z = x * y;
+            return z;
+        }
+        public double Divide(double x, double y) {
+            double z = x / y;
+            return z;
+        }
+        public long Modulo(long x, long y) {
+            long z = x % y;
+            return z;
+        }
+    }
+}
