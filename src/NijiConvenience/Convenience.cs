eebd02d693617a37d3ae1f85367b15e08fba1ab2
Satoki Nijikawa <120293916+NijikawaSatoki@users.noreply.github.com>
2024-10-30 11:33:06 -0400

    Update Convenience.cs
    Added clarifying spaces.
    
    ---Satoki
    [v1.1.0]

--- src/NijiConvenience/Convenience.cs
+++ src/NijiConvenience/Convenience.cs
@@ -14,18 +14,22 @@ namespace NijiConvenience {
             bool booleanState = Convert.ToBoolean(Console.ReadLine());
             return booleanState;
         }
+        
         public char InputCharacter() {
             char character = Convert.ToChar(Console.ReadLine());
             return character;
         }
+        
         public double InputDouble() {
             double doubleFloat = Convert.ToDouble(Console.ReadLine());
             return doubleFloat;
         }
+        
         public int Input32BitInteger() {
             int int32 = Convert.ToInt32(Console.ReadLine());
             return int32;
         }
+        
         public long Input64BitInteger() {
             long int64 = Convert.ToInt64(Console.ReadLine());
             return int64;
