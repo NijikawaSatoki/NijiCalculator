233781594c338f35b62bc376ab80073ec9a25e42
Satoki Nijikawa <120293916+NijikawaSatoki@users.noreply.github.com>
2024-10-30 11:30:55 -0400

    Update MainProgram.cs
    Put the curly brackets back on the same lines as their statements; added clarifying spaces.
    
    ---Satoki
    [v1.1.0]

--- src/MainProgram.cs
+++ src/MainProgram.cs
@@ -21,9 +21,11 @@ namespace NijiCalculator {
             const char BLANK_LINE = ' ';
             const char SINGLE_TAB = '\t';
             const string DOUBLE_TAB = "\t\t";
+            
             // Variables
             char response = ' ';
             string operation = " ";
+            
             // 1D arrays
             string[,] operations = new string[3, 8] {
                 {
@@ -37,98 +39,113 @@ namespace NijiCalculator {
                     "addition", "subtraction", "multiplication", "division", "modulus", "exponent", "#@%$!", "#@%$!"
                 }
             };
+            
             // Objects
             Convenience convenience = new Convenience(); // ./NijiConvenience/Convenience.cs
             Calculate calculate = new Calculate(); // ./NijiCalculator/Calculate.cs
+            
             // Introduction
             Console.WriteLine($"{SINGLE_TAB}\"Niji's Calculator\"");
             Console.WriteLine($"{DOUBLE_TAB}By Niji System");
             Console.WriteLine(BLANK_LINE);
             Console.WriteLine("This is a calculator written in C#.");
+            
             // The main calculator part :3
             do {
                 // Math variables
                 long x = 0;
                 long y = 0;
                 double yDivide = 0.000000000000000;
+                
                 // This controls whether or not the DividedByZero code runs.
                 bool divideByZero = false;
                 Console.WriteLine("What operation would you like to perform?");
                 Console.WriteLine("Your current options are:");
+                
                 // List of available operations
                 Console.WriteLine($"{operations[0, 0]}{SINGLE_TAB}{operations[0, 1]}{SINGLE_TAB}{operations[0, 2]}{LINE_BREAK}{operations[0, 3]}{SINGLE_TAB}{operations[0, 4]}{DOUBLE_TAB}{operations[0, 5]}{LINE_BREAK}Exit");
+                
                 // Retrieve user input
                 Console.Write("Please enter your selection: ");
                 operation = Console.ReadLine();
+                
                 // Check against the available operations
-                if (operation == operations[0, 0] || operation == operations[1, 0] || operation == operations[2, 0])
-                {
+                if (operation == operations[0, 0] || operation == operations[1, 0] || operation == operations[2, 0]) {
                     /* <3 `Addition' <3 */
                     Console.WriteLine($"You chose {operations[2, 0]}.");
+                    
                     // Initialise variables
                     long sum = 0;
+                    
                     // Explanation
                     Console.WriteLine("Addition adds two numbers together.");
+                    
                     // Grab user input
                     Console.Write("Please enter a number (no decimals please): ");
                     x = convenience.Input64BitInteger();
                     Console.Write("Please enter a second number (again, no decimals please): ");
                     y = convenience.Input64BitInteger();
+                    
                     // Calculate the sum
                     sum = calculate.Add(x, y);
                     Console.WriteLine($"Your sum is {sum}.");
-                }
-                else if (operation == operations[0, 1] || operation == operations[1, 1] || operation == operations[2, 1])
-                {
+                } else if (operation == operations[0, 1] || operation == operations[1, 1] || operation == operations[2, 1]) {
                     /* <3 `Subtraction' <3 */
                     Console.WriteLine($"You chose {operations[2, 1]}.");
+                    
                     // Initialise variables
                     long difference = 0;
+                    
                     // Explanation
                     Console.WriteLine("Subtraction subtracts one number from another.");
+                    
                     // Grab user input
                     Console.Write("Please enter a number (no decimals please): ");
                     x = convenience.Input64BitInteger();
                     Console.Write("Please enter the amount to take away (again, no decimals please): ");
                     y = convenience.Input64BitInteger();
+                    
                     // Calculate the difference
                     difference = calculate.Subtract(x, y);
                     Console.WriteLine($"Your difference is {difference}.");
-                }
-                else if (operation == operations[0, 2] || operation == operations[1, 2] || operation == operations[2, 2])
-                {
+                } else if (operation == operations[0, 2] || operation == operations[1, 2] || operation == operations[2, 2]) {
                     /* <3 `Multiplication' <3 */
                     Console.WriteLine($"You chose {operations[2, 2]}.");
+                    
                     // Initialise variables
                     long product = 0;
+                    
                     // Explanation
                     Console.WriteLine("Multiplication multiplies a number by another number.");
+                    
                     // Grab user input
                     Console.Write("Please enter a number (no decimals please): ");
                     x = convenience.Input64BitInteger();
                     Console.Write("Please enter the number you want to multiply by (again, no decimals please): ");
                     y = convenience.Input64BitInteger();
+                    
                     // Calculate the product
                     product = calculate.Multiply(x, y);
                     Console.WriteLine($"Your product is {product}.");
-                }
-                else if (operation == operations[0, 3] || operation == operations[1, 3] || operation == operations[2, 3])
-                {
+                } else if (operation == operations[0, 3] || operation == operations[1, 3] || operation == operations[2, 3]) {
                     /* <3 `Division' <3 */
                     Console.WriteLine($"You chose {operations[2, 3]}.");
+                    
                     // Initialise variables
                     double xDivide = 0.000000000000000;
                     double quotient = 0.000000000000000;
+                    
                     // Explanation
                     Console.WriteLine("Division divides one number by another.");
+                    
                     // Grab user input
                     Console.Write("Please enter a number: ");
                     xDivide = convenience.InputDouble();
                     Console.Write("Please enter the number you want divide by: ");
                     yDivide = convenience.InputDouble();
+                    
                     // Check if the divisor is 0
-                    if (yDivide == 0)
-                    {
+                    if (yDivide == 0) {
                         divideByZero = true;
                         // You destroyed the universe...
                         Console.WriteLine("WARNING! USER ATTEMPTED TO DIVIDE BY ZERO!");
@@ -136,9 +153,7 @@ namespace NijiCalculator {
                         Console.WriteLine("Are you happy with yourself?");
                         Console.WriteLine("You just destroyed the entire universe by creating a singularity.");
                         goto DividedByZero;
-                    }
-                    else if (yDivide != 0)
-                    {
+                    } else if (yDivide != 0) {
                         // Calculate the quotient
                         quotient = calculate.Divide(xDivide, yDivide);
                         Console.WriteLine($"Your quotient is {quotient}.");
@@ -146,18 +161,21 @@ namespace NijiCalculator {
                 } else if (operation == operations[0, 4] || operation == operations[1, 4] || operation == operations[2, 4]) {
                     /* <3 `Modulus' <3 */
                     Console.WriteLine($"You chose {operations[2, 4]}.");
+                    
                     // Initialise variables
                     long remainder = 0;
+                    
                     // Explanation
                     Console.WriteLine("Modulus takes the remainder from a division operation.");
+                    
                     // Grab user input
                     Console.Write("Please enter a number (no decimals please): ");
                     x = convenience.Input64BitInteger();
                     Console.Write("Please enter the number you want to divide by (again, no decimals please): ");
                     y = convenience.Input64BitInteger();
+                    
                     // Check if the divisor is 0
-                    if (y == 0)
-                    {
+                    if (y == 0) {
                         divideByZero = true;
                         // You destroyed the universe...
                         Console.WriteLine("WARNING! USER ATTEMPTED TO DIVIDE BY ZERO!");
@@ -165,9 +183,7 @@ namespace NijiCalculator {
                         Console.WriteLine("Are you happy with yourself?");
                         Console.WriteLine("You just destroyed the entire universe by creating a singularity.");
                         goto DividedByZero;
-                    }
-                    else if (y != 0)
-                    {
+                    } else if (y != 0) {
                         // Retrieve the remainder
                         remainder = calculate.Modulo(x, y);
                         Console.WriteLine($"The remainder from {x} divided by {y} is {remainder}.");
@@ -175,17 +191,21 @@ namespace NijiCalculator {
                 } else if (operation == operations[0, 5] || operation == operations[1, 5] || operation == operations[2, 5]) {
                     /* <3 `nth Power Exponent' <3 */
                     Console.WriteLine($"You chose {operations[2, 5]}.");
+                    
                     // Initialise variables
                     double exponentBase = 1.00;
                     double exponentPower = 0.00;
                     double powerResult = 1.00;
+                    
                     // Explanation
                     Console.WriteLine("Exponents simply denoted a number being multiplied by itself x amount of times.");
+                    
                     // Grab user input
                     Console.Write("Please enter your base: ");
                     exponentBase = convenience.Input64BitInteger();
                     Console.Write("Please enter your exponent: ");
                     exponentPower = convenience.Input64BitInteger();
+                    
                     // Calculate the result
                     powerResult = calculate.NthPower(exponentBase, exponentPower);
                     Console.WriteLine($"{exponentBase} to the power of {exponentPower} results in {powerResult}.");
@@ -197,6 +217,7 @@ namespace NijiCalculator {
                     /* <3 Invalid input <3 */
                     Console.WriteLine(operations[0, 7]); // #@%$!
                 }
+                
                 DividedByZero:
                     if (divideByZero == true) {
                         Console.WriteLine("*ugh*");
@@ -204,10 +225,12 @@ namespace NijiCalculator {
                         Console.WriteLine("Next time, DON'T DIVIDE BY ZERO!");
                     }
                 /* oreZyBdediviD */
+                
                 // Ask the user what they want to do
                 Console.Write("Would you like to perform another operation? [Y/n] ");
                 response = convenience.InputCharacter();
             } while (response == 'Y' || response == 'y');
+            
             // Sendoff
             if (response == 'N' || response == 'n') {
                 Console.WriteLine("Have a nice day!");
