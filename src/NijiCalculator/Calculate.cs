9bb55007680f22ee4ea877357a8e4c086928e252
Satoki Nijikawa <120293916+NijikawaSatoki@users.noreply.github.com>
2024-10-30 11:32:21 -0400

    Update Calculate.cs
    Added clarifying spaces.
    
    ---Satoki
    [v1.1.0]

--- src/NijiCalculator/Calculate.cs
+++ src/NijiCalculator/Calculate.cs
@@ -11,27 +11,34 @@ using System;
 namespace NijiCalculator {
     class Calculate {
         // Basic functions
+        
         public long Add(long x, long y) {
             long z = x + y;
             return z;
         }
+        
         public long Subtract(long x, long y) {
             long z = x - y;
             return z;
         }
+        
         public long Multiply(long x, long y) {
             long z = x * y;
             return z;
         }
+        
         public double Divide(double x, double y) {
             double z = x / y;
             return z;
         }
+        
         public long Modulo(long x, long y) {
             long z = x % y;
             return z;
         }
+        
         // Exponential functions
+        
         public double NthPower(double x, double y) {
             double z = Math.Pow(x, y);
             return z;
