43675b09d8b341e0d71788462bf18fc69929f2c7
NijikawaSatoki <nijikawasatoki00@outlook.com>
2024-11-04 00:13:53 -0500

    Updated the build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 # This script simply runs mono's compiler
 
-mcs src/MainProgram.cs src/NijiCalculator/Calculate.cs src/NijiConvenience/Convenience.cs -out:test/newTest00
+mcs src/MainProgram.cs src/NijiCalculator/Calculate.cs src/NijiConvenience/Convenience.cs -out:test/newTest09
