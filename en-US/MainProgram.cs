5f23190fe704d8af8a79d9924b2e69564aee9473
Satoki Nijikawa <nijikawasatoki00@outlook.com>
2026-07-16 21:33:25 -0400

    Calculator is now a proper project

--- en-US/MainProgram.cs
+++ en-US/MainProgram.cs
@@ -30,17 +30,14 @@ namespace NijiCalculator
             const string AUTHOR = "Niji System\n\n";
             Console.Write($"{TITLE}{AUTHOR}");
             // Decisions...
-            SetUpList:
-            {
-                List<string> options = new List<string>();
-                options.Add("Addition");          // Option 0
-                options.Add("Subtraction");       // Option 1
-                options.Add("Multiplication");    // Option 2
-                options.Add("Division");          // Option 3
-                options.Add("Modulus");           // Option 4
-                options.Add("Summation");         // Option 5
-            }
-            string optionList = $"";
+            List<string> options = new List<string>();
+            options.Add("Addition");          // Option 0
+            options.Add("Subtraction");       // Option 1
+            options.Add("Multiplication");    // Option 2
+            options.Add("Division");          // Option 3
+            options.Add("Modulus");           // Option 4
+            options.Add("Summation");         // Option 5
+            string optionList = $"0. {options[0]}\t1. {options[1]}\t2. {options[2]}\n3. {options[3]}\t4. {options[4]}\t5. {options[5]}";
             Console.WriteLine("Welcome!");
             Console.WriteLine(optionList);
             Console.Write("What do you want to do? ");
@@ -74,11 +71,11 @@ namespace NijiCalculator
                     break;
                 case 3:     // Division
                     Console.Write("Enter a number: ");
-                    int divideX = Convert.ToInt32(Console.ReadLine());
+                    double divideX = Convert.ToDouble(Console.ReadLine());
                     Console.Write("Enter another number: ");
-                    int divideY = Convert.ToInt32(Console.ReadLine());
-                    int divideResult = math.Division(divideX, divideY);
-                    Console.WriteLine($"{divideX} * {divideY} = {divideResult}");
+                    double divideY = Convert.ToDouble(Console.ReadLine());
+                    double divideResult = math.Division(divideX, divideY);
+                    Console.WriteLine($"{divideX} / {divideY} = {divideResult}");
                     break;
                 case 4:     // Modulus
                     Console.Write("Enter a number: ");
@@ -86,13 +83,14 @@ namespace NijiCalculator
                     Console.Write("Enter another number: ");
                     int moduloY = Convert.ToInt32(Console.ReadLine());
                     int moduloResult = math.Modulus(moduloX, moduloY);
-                    Console.WriteLine($"{moduloX} * {moduloY} = {moduloResult}");
+                    Console.WriteLine($"{moduloX} % {moduloY} = {moduloResult}");
                     break;
                 case 5:     // Summation
                     Console.Write("Enter a number: ");
                     int sumX = Convert.ToInt32(Console.ReadLine());
                     int sumResult = math.Summation(sumX);
-                    Console.WriteLine($"Σ({sumX}) = {sumResult}");
+                    Console.WriteLine($"∑({sumX}) = {sumResult}");
+                    break;
                 default:    // Out of bounds
                     Console.WriteLine("Out of bounds!");
                     Console.WriteLine("Stop using that damned noclip!");
