36b2e5f939614a0660803cba1e2eed6800f9f96d
Satoki Nijikawa <nijikawasatoki00@outlook.com>
2026-09-11 15:37:33 -0400

    Added methods for read unsigned integer inputs; also added a way to inform the user that a feature is not implemented yet.

--- NijiConvenience.cs
+++ NijiConvenience.cs
@@ -20,6 +20,8 @@ namespace NijiCalculator
 {
     class NijiConvenience
     {
+        private string _notImplemented = "This is not implemented yet!";
+        public string NotImplemented => _notImplemented;
         // Read in specific input types
         public bool ReadBooleanInput()
         {
@@ -41,10 +43,20 @@ namespace NijiCalculator
             int integer32Bit = Convert.ToInt32(Console.ReadLine());
             return integer32Bit;
         }
+        public uint ReadUnsigned32BitNumberInput()
+        {
+            uint unsignedInteger32Bit = (uint)Convert.ToInt32(Console.ReadLine());
+            return unsignedInteger32Bit;
+        }
         public long Read64BitNumberInput()
         {
             long integer64Bit = Convert.ToInt64(Console.ReadLine());
             return integer64Bit;
+        }
+        public ulong ReadUnsigned64BitNumberInput()
+        {
+            ulong unsignedInteger64Bit = (ulong)Convert.ToInt64(Console.ReadLine());
+            return unsignedInteger64Bit;
         }
     }
 }
